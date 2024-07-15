d9149f92c5dbda05a693ed602618e807dbdbedb5
rahuljsaliaan <rahuljsaliaan@gmail.com>
2024-07-15 11:11:49 +0530

    initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh"],
+  rules: {
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+    "no-console": ["warn"],
+    "no-unused-vars": [
+      "error",
+      {
+        vars: "all",
+        args: "after-used",
+        ignoreRestSiblings: true,
+        varsIgnorePattern: "^_",
+        argsIgnorePattern: "^_",
+      },
+    ],
+  },
+};
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Count from "@/components/Count/Count";
+import "@/styles/app.css";
+
+export default function App() {
+  return (
+    <main className="center">
+      <h1>Trackademic UI</h1>
+
+      <Count />
+    </main>
+  );
+}
