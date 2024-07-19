506b535a655b87bdb5a6b88410ed9e475c7f3154
shravanss <shravanappu2001@gmail.com>
2024-07-20 01:31:29 +0530

    style(ForgotResetPassword): :lipstick: Create ForgotResetPassword Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import "@/styles/app.css";
 // import GetStarted from "@/pages/getStarted/GetStarted";
 // import Login from "@/pages/auth/Login";
-import ForgotPassword from "@/pages/auth/ForgotPassword";
+// import ForgotPassword from "@/pages/auth/ForgotPassword";
+import ForgotResetPassword from "@/pages/auth/ForgotResetPassword";
 
 export default function App() {
   return (
     <main className="">
       {/* <GetStarted /> */}
-      <ForgotPassword/>
+      <ForgotResetPassword/>
     </main>
   );
 }
