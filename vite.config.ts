c287be2b6319c3b246cb82b578d92e3956773aa2
rahuljsaliaan <rahuljsaliaan@gmail.com>
2024-08-01 21:35:00 +0530

    fix(submodule): :bug: Submodule env bug fixed

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,12 @@ import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 import path from 'path';
 import react from '@vitejs/plugin-react-swc';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
   plugins: [
     react(),
+    nodePolyfills(),
     VitePWA({
       registerType: 'autoUpdate',
       workbox: {
